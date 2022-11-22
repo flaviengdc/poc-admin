c3602d2febb1db42be9cc57c9d488a9a04d7884b
flavien <flavien@gensdeconfiance.com>
2022-11-22 15:46:21 +0100

    NocoDB - Init
    Disable CORS for NocoDB

---
+++ nocodb/index.js
@@ -0,0 +1,12 @@
+(async () => {
+  try {
+    const app = require("express")();
+    app.use(require("cors")());
+    const { Noco } = require("nocodb");
+    const httpServer = app.listen(process.env.PORT || 8080);
+    app.use(await Noco.init({}, httpServer, app));
+    console.log(`Visit : localhost:${process.env.PORT}/dashboard`);
+  } catch (e) {
+    console.log(e);
+  }
+})();
