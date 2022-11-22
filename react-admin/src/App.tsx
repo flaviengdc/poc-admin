a842f5fed41a70b4f085afbc731ead160bebffaa
flavien <flavien@gensdeconfiance.com>
2022-11-22 14:56:56 +0100

    React-Admin - Init
    Steal ra-data-simple-rest

---
+++ react-admin/src/App.tsx
@@ -0,0 +1,8 @@
+// import jsonServerProvider from "ra-data-json-server";
+import { Admin } from "react-admin";
+
+// const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+const App = () => <Admin dataProvider={dataProvider} />;
+
+export default App;
